d543123e25e2f184f6ef25e60f8e6572e29bb906
Pizza <105643708+WildePizza@users.noreply.github.com>
2024-07-14 17:28:10 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -32,7 +32,7 @@ pat=$3
 
 execute() {
   substring="#!/bin/bash"
-  sha=$(curl -sSL https://api.github.com/repos/WildePizza/kubernetes-dashboard/commits?per_page=2 | jq -r '.[1].sha')
+  sha=$(curl -sSL https://api.github.com/repos/WildePizza/kubernetes-center/commits?per_page=2 | jq -r '.[1].sha')
   url="https://raw.githubusercontent.com/WildePizza/$repo/HEAD/.commits/$sha/scripts/$action.sh"
   echo "Running script: $url"
   output=$(curl -fsSL $url 2>&1)
